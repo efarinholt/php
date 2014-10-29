87f92ac855e557122304dbf7f1a33b89477ebe32
Evan Farinholt <evan@fireinspace.net>
2014-10-29 16:15:37 -0400

    Update emailMessage.php
    Send a simple email using PHP

--- emailMessage.php
+++ emailMessage.php
@@ -1,7 +1,10 @@
 <?php
 
-$headers .= 'From: FIS Admin <evan@fireinspace.net>' . "\r\n";
+// define email headers
+$headers .= 'From: FIS Admin <evan@fireinspace.net>' . "\r\n"; 
+// define the content of the email
 $submit_message = "Error in following record: \n Email: $user_email \n Phone: $user_full_phone \n Post URL: $post_url";
+// send the message
 mail('evan@fireinspace.net', 'Error', $submit_message, $headers);
 
 ?>

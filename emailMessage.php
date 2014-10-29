77536a7629da745af16a668913827aeba474c151
Evan Farinholt <evan@fireinspace.net>
2014-10-29 17:31:26 -0400

    Update emailMessage.php

--- emailMessage.php
+++ emailMessage.php
@@ -1,10 +1,10 @@
 <?php
-
-// define email headers
-$headers .= 'From: FIS Admin <evan@fireinspace.net>' . "\r\n"; 
-// define the content of the email
-$submit_message = "Error in following record: \n Email: $user_email \n Phone: $user_full_phone \n Post URL: $post_url";
-// send the message
-mail('evan@fireinspace.net', 'Error', $submit_message, $headers);
-
+function logFile($user_email, $user_phone, $admin_email){
+    // define email headers
+    $headers .= 'From: FIS Admin <' . $admin_email . '>' . "\r\n"; 
+    // define the content of the email
+    $submit_message = "Error in following record: \n Email: $user_email \n Phone: $user_phone \n Post URL: $post_url";
+    // send the message
+    mail($admin_email, 'Error', $submit_message, $headers);
+}
 ?>

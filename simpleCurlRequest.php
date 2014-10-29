127d15f15fcf7a84b0203b916f1809341e718544
Evan Farinholt <evan@fireinspace.net>
2014-10-29 17:42:00 -0400

    Update simpleCurlRequest.php

--- simpleCurlRequest.php
+++ simpleCurlRequest.php
@@ -1,12 +1,16 @@
 <?php
 
+function curlRequest($someVariable, $anotherVariable){
 $url_request = 'http://api.url.com?aaa=' . $someVariable . '&bbb=' . $anotherVariable;
 
 $curl = curl_init();
-  curl_setopt($curl, CURLOPT_URL, $url_request);
-  curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl, CURLOPT_URL, $url_request);
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 $response = curl_exec($curl);
-  curl_close($curl);
+curl_close($curl);
 $xml = simplexml_load_string($response);
+
+return $xml;
+}
 
 ?>

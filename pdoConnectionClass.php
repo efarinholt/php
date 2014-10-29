75c1bdc545d459d35cc53a3363d02e8f3f48c081
Evan Farinholt <evan@fireinspace.net>
2014-10-29 16:56:13 -0400

    Update pdoConnectionClass.php

--- pdoConnectionClass.php
+++ pdoConnectionClass.php
@@ -1,20 +1,19 @@
 <?php
 
-//connection class
-class Connection{
-	public function dbConnect(){
-		return new PDO("mysql:host=SERVER_URL; dbname=DB_NAME", "USER_NAME", "PASSWORD");
+class Connection{ // define class name
+	public function dbConnect(){ // define function name
+		// connect via pdo and define server url, db name, username, and password
+		return new PDO("mysql:host=SERVER_URL; dbname=DB_NAME", "USER_NAME", "PASSWORD"); 
 	}
 }
 
-//other class
-class otherClass{
+class anotherClass{
+	// call within another class to establish connection
 	private $db;	
 	public function __construct(){
 		$this->db = new Connection();
 		$this->db = $this->db->dbConnect();
 	}
-	//the rest of your code...
 }
 
 ?>

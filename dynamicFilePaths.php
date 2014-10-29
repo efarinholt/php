b64f0a7fc322cb73d9d8557c23949eca96fbfed3
Evan Farinholt <evan@fireinspace.net>
2014-10-29 17:49:41 -0400

    Update dynamicFilePaths.php

--- dynamicFilePaths.php
+++ dynamicFilePaths.php
@@ -4,7 +4,7 @@ $project = 'myProject';
 $round = 'r4';
 
 if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) { // if file is called locally (local ip address)
-    $root = $_SERVER['DOCUMENT_ROOT'] . "/$project/";
+	$root = $_SERVER['DOCUMENT_ROOT'] . "/$project/";
 	$metaroot = "/$project/";
 }else{ // else define remote file path
 	$root = "/home/170115/domains/fisdev.fireinspace.net/html/Q3M_Design/$project/$round/";

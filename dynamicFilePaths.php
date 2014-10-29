941b8aed4a960bf11d7939ebef60c63212fb477d
Evan Farinholt <evan@fireinspace.net>
2014-10-29 16:11:26 -0400

    Update dynamicFilePaths.php
    Set variables based on the location of the file, which can be called when defining document locations

--- dynamicFilePaths.php
+++ dynamicFilePaths.php
@@ -3,10 +3,10 @@
 $project = 'myProject';
 $round = 'r4';
 
-if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
+if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) { // if file is called locally (local ip address)
     $root = $_SERVER['DOCUMENT_ROOT'] . "/$project/";
 	$metaroot = "/$project/";
-}else{
+}else{ // else define remote file path
 	$root = "/home/170115/domains/fisdev.fireinspace.net/html/Q3M_Design/$project/$round/";
 	$metaroot = "http://fisdev.fireinspace.net/Q3M_Design/$project/$round/";
 }

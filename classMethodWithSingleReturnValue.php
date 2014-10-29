90175fa127320280698ef639251a878824930cf0
Evan Farinholt <evan@fireinspace.net>
2014-10-29 17:11:39 -0400

    Create classMethodWithSingleReturnValue.php

---
+++ classMethodWithSingleReturnValue.php
@@ -0,0 +1,20 @@
+<?php
+
+class myClass{ // define class name
+	public function myFunction($x){ // define function name and expected incoming parameter
+		
+		// conditions of function
+		if($x == 'a'){
+			$variable = '1';
+		}else{
+			$variable = '2';
+		}
+		
+		return $variable; // return a value
+	}
+}
+
+$object = new myClass(); // call the class
+$variable = $object->myFunction($x); // call the function and get returned value
+
+?>

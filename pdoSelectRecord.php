2810c992312ebffa51e4faa4918a90e699956b48
Evan Farinholt <evan@fireinspace.net>
2014-10-31 16:53:49 -0400

    Create pdoSelectRecord.php

---
+++ pdoSelectRecord.php
@@ -0,0 +1,14 @@
+<?php	
+    require('connection.php'); // connection class is required
+	
+	$st = $handler->prepare("SELECT * FROM tbl_users"); // select all records from the table 'tbl_users'
+	$st->bindColumn('user_first', $user_first); // bind the columns to variables
+	$st->bindColumn('user_last', $user_last);
+	$st->bindColumn('user_email', $user_email);
+	$st->execute(); // execute the query
+	
+	while ($st->fetch(PDO::FETCH_BOUND)) { // execute action on the fetched values
+	  echo "<span>Hello $user_first $user_last!</span>\n";
+	  echo "<span>Email: $user_email</span>";
+	}
+?>

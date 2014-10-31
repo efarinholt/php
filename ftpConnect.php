0f841c5e50e3573b21f809e9ecb7db98dd8a82c8
Evan Farinholt <evan@fireinspace.net>
2014-10-31 17:14:26 -0400

    Update ftpConnect.php

--- ftpConnect.php
+++ ftpConnect.php
@@ -1,9 +1,5 @@
 <?php 
-function ftpConnect(){
-	$ftp_server = "SERVER_URL";
-	$ftp_user = "FTP_USERNAME";
-	$ftp_pass = "FTP_PASSWORD";
-			
+function ftpConnect($ftp_server, $ftp_user, $ftp_pass){
 	$conn_id = ftp_connect($ftp_server) or die("Couldn't connect to $ftp_server"); 
 	if (@ftp_login($conn_id, $ftp_user, $ftp_pass)) {
 		$connected = true;

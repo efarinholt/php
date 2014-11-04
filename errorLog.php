434bdb5633dd67fcb864eb49132c9a28d6f667d2
Evan Farinholt <evan@fireinspace.net>
2014-11-04 12:05:07 -0500

    Update errorLog.php

--- errorLog.php
+++ errorLog.php
@@ -1,6 +1,6 @@
 <?php
-funciton fx_LogAction($user_email, $user_name, $user_phone){
-    $add_to_log = "$user_email|$user_name|$user_phone\n"; // define contents of log entry
+funciton fx_LogAction($user_email, $user_name, $user_phone, $user_action){
+    $add_to_log = "$user_email|$user_name|$user_phone|$user_action\n"; // define contents of log entry
     $log_file = 'db_Log.txt'; // define log file location
     $fp = fopen($log_file, "a+"); // open log file
     fputs($fp, $add_to_log); // write to log file

d127e22c37c4507c03588e420d5e6f0b74c31a03
Evan Farinholt <evan@fireinspace.net>
2014-11-04 12:02:09 -0500

    Update createNewDirectory.php

--- createNewDirectory.php
+++ createNewDirectory.php
@@ -1,5 +1,5 @@
 <?php
-function newDirectory($dirName){ // define the function name and expected incoming parameter
+function fx_NewDirectory($dirName){ // define the function name and expected incoming parameter
 	$newDirectory = mkdir ('/path/to/file/' . $dirName , 0777); // create the directory and set the permissions
 	return $newDirectory; // return the directory name
 }

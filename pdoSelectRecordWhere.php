2730a2bcf2e769ed56101bb3fa0bf813a852efa9
Evan Farinholt <evan@fireinspace.net>
2014-11-04 11:09:56 -0500

    Update pdoSelectRecordWhere.php

--- pdoSelectRecordWhere.php
+++ pdoSelectRecordWhere.php
@@ -1,13 +1,13 @@
 <?php	
 require('pdoConnectionClass.php'); // connection class is required for pdo handler
 
-class userEval{
+class cl_userEval{
 	private $db;
 	public function __construct(){
 		$this->db = new Connection();
 		$this->db = $this->db->dbConnect();
 	}
-	function pdoSelect($user_email){ // define the function and expected incoming parameter
+	function fx_pdoSelect($user_email){ // define the function and expected incoming parameter
 		try {
 			// selects all records that match the incoming variable 
 			$st = $handler->prepare("SELECT * FROM tbl_users WHERE user_email = '$user_email'"); 
@@ -22,10 +22,8 @@ class userEval{
 		}catch(PDOException $e){
 			print $e->getMessage();
 		}
-	}
-}
-
-/****/
+	} // END fx_pdoSelect
+} // END cl_userEval
 
 $object = new userEval(); // call the function in later code
 list($user_first, $user_last, $user_email) = $object->pdoSelect($user_email); // assign return values to list variables

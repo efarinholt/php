1fb93472f6d90c5a2d4fc7a7c0b9db44916b65a0
Evan Farinholt <evan@fireinspace.net>
2014-11-04 11:15:59 -0500

    Update pdoUpdateRecordWhere.php

--- pdoUpdateRecordWhere.php
+++ pdoUpdateRecordWhere.php
@@ -1,19 +1,29 @@
 <?php
 require('pdoConnectionClass.php'); // connection class required for pdo handler
 
-class userUpdate{ // define class name
-    function pdoUpdate($user_email){ // define function name and expected incoming parameter
-        // update records that match the incoming variable
-        $st = $handler->prepare("
-            UPDATE tbl_users
-            SET user_first=?, user_last=?, user_phone=?
-            WHERE user_email ='$user_email'
-        ");
-        
-        $st->bindParam(1, $user_first);
-        $st->bindParam(2, $user_last);
-        $st->bindParam(3, $user_phone);
-        $st->execute();
+class cl_userUpdate{ // define class name
+    private $db;
+    public function __construct(){
+    	$this->db = new Connection();
+    	$this->db = $this->db->dbConnect();
     }
-}
+    function fx_pdoUpdate($user_email){ // define function name and expected incoming parameter
+        try{
+            // update records that match the incoming variable
+            $st = $handler->prepare("
+                UPDATE tbl_users
+                SET user_first=?, user_last=?, user_phone=?
+                WHERE user_email ='$user_email'
+            ");
+            
+            $st->bindParam(1, $user_first);
+            $st->bindParam(2, $user_last);
+            $st->bindParam(3, $user_phone);
+            $st->execute();
+            
+        }catch(PDOException $e){
+            print $e->getMessage();
+        }
+    } // END fx_pdoUpdate
+} // END cl_userUpdate
 ?>

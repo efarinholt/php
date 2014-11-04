e3fca3dba5b012938ae431a71c8f9f4b75f01cd7
Evan Farinholt <evan@fireinspace.net>
2014-11-04 11:05:55 -0500

    Update pdoConnectionClass.php

--- pdoConnectionClass.php
+++ pdoConnectionClass.php
@@ -2,18 +2,8 @@
 
 class Connection{ // define class name
 	public function dbConnect(){ // define function name
-		// connect via pdo and define server url, db name, username, and password
-		return new PDO("mysql:host=SERVER_URL; dbname=DB_NAME", "USER_NAME", "PASSWORD"); 
+		return new PDO("mysql:host=localhost; dbname=db_DatabaseName", "root", "root");
 	}
-}
-
-try{//use pdo to catch errors
-	$handler = new Connection();
-	$handler = $handler->dbConnect();
-	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-	echo $e->getMessage();
-	die();
 }
 
 ?>

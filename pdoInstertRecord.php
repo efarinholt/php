aeea9f9ea3fbbb9cefc6116564b9cc82d649f13e
Evan Farinholt <evan@fireinspace.net>
2014-11-04 11:12:59 -0500

    Update pdoInstertRecord.php

--- pdoInstertRecord.php
+++ pdoInstertRecord.php
@@ -9,19 +9,21 @@ class cl_pdoInstert{ // define class name
 		$this->db = $this->db->dbConnect();
 	}
 	public function fx_instertRecord($user_email, $user_first, $user_last){ // define function name and expected incoming parameters
-			
-		$st = $handler->prepare( // sql insert query, which defines the table name and field names that will recieve the content
-			"INSERT INTO tbl_users 
-			(user_email, user_first, user_last) 
-			VALUES 
-			(?,?,?)"
-		);
-		// bind parameters to the anonymized insert values from line 11
-		$st->bindParam(1, $user_email); 
-		$st->bindParam(2, $user_first);
-		$st->bindParam(3, $user_last);
-		$st->execute(); // execute the sql insert
-			
+		try {	
+			$st = $handler->prepare( // sql insert query, which defines the table name and field names that will recieve the content
+				"INSERT INTO tbl_users 
+				(user_email, user_first, user_last) 
+				VALUES 
+				(?,?,?)"
+			);
+			// bind parameters to the anonymized insert values from line 11
+			$st->bindParam(1, $user_email); 
+			$st->bindParam(2, $user_first);
+			$st->bindParam(3, $user_last);
+			$st->execute(); // execute the sql insert
+		}catch(PDOException $e){
+			print $e->getMessage();
+		}	
 	} // END fx_instertRecord
 } // END cl_pdoInstert
 

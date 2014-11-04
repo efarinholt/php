8b1fe9bfab73a348a72abb9440394d893e742d2f
Evan Farinholt <evan@fireinspace.net>
2014-11-04 11:05:02 -0500

    Update pdoSelectRecordWhere.php

--- pdoSelectRecordWhere.php
+++ pdoSelectRecordWhere.php
@@ -3,16 +3,24 @@ require('pdoConnectionClass.php'); // connection class is required for pdo handl
 
 class userEval{
 	function pdoSelect($user_email){ // define the function and expected incoming parameter
-		// selects all records that match the incoming variable 
-		$st = $handler->prepare("SELECT * FROM tbl_users WHERE user_email = '$user_email'"); 
-		$st->bindColumn('user_first', $user_first); // bind the matched columns to variables
-		$st->bindColumn('user_last', $user_last);
-		$st->bindColumn('user_email', $user_email);
-		$st->execute(); // execute the query
-		
-		return array($user_first, $user_last, $user_email); // results to be returned when class method is called
+		try {
+			// selects all records that match the incoming variable 
+			$st = $handler->prepare("SELECT * FROM tbl_users WHERE user_email = '$user_email'"); 
+			$st->bindColumn('user_first', $user_first); // bind the matched columns to variables
+			$st->bindColumn('user_last', $user_last);
+			$st->bindColumn('user_email', $user_email);
+			$st->execute(); // execute the query
+			
+			while($st->fetch(PDO::FETCH_BOUND)){
+				return array($user_first, $user_last, $user_email); // results to be returned when class method is called
+			}
+		}catch(PDOException $e){
+			print $e->getMessage();
+		}
 	}
 }
+
+/****/
 
 $object = new userEval(); // call the function in later code
 list($user_first, $user_last, $user_email) = $object->pdoSelect($user_email); // assign return values to list variables

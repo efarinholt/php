49e116b1df4cb748dbe8710d1325bc67890c2524
Evan Farinholt <evan@fireinspace.net>
2014-11-04 11:11:22 -0500

    Update pdoInstertRecord.php

--- pdoInstertRecord.php
+++ pdoInstertRecord.php
@@ -2,13 +2,13 @@
 
 require('pdoConnectionClass.php'); // connection class is required for pdo handler
 
-class pdoInstert{ // define class name
+class cl_pdoInstert{ // define class name
 	private $db;
 	public function __construct(){
 		$this->db = new Connection();
 		$this->db = $this->db->dbConnect();
 	}
-	public function instertRecord($user_email, $user_first, $user_last){ // define function name and expected incoming parameters
+	public function fx_instertRecord($user_email, $user_first, $user_last){ // define function name and expected incoming parameters
 			
 		$st = $handler->prepare( // sql insert query, which defines the table name and field names that will recieve the content
 			"INSERT INTO tbl_users 
@@ -22,10 +22,9 @@ class pdoInstert{ // define class name
 		$st->bindParam(3, $user_last);
 		$st->execute(); // execute the sql insert
 			
-	}
-}
+	} // END fx_instertRecord
+} // END cl_pdoInstert
 
-/****/
 
 $object = new pdoInstert(); // call the class name
 $object->instertRecord($user_email, $user_first, $user_last); // call the function and define the variables to insert

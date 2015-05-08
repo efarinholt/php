fdcd34bf51689079abe852609db9950dca2dd4a6
Evan Farinholt <evan@fireinspace.net>
2015-05-08 14:57:48 -0400

    Update curlWebservice.php

--- curlWebservice.php
+++ curlWebservice.php
@@ -1,20 +1,56 @@
+<?php
+
+/*
+the curl webservice takes information from a url that is passed to it and sends a response - 
+in this case, the response is a JSON object
+
+for the sake of this example, the webservice url is:
+http://evanfarinholt.com/api.php
+
+we pass information via url-encoded parameters. so our final URL post will be:
+http://evanfarinholt.com/api.php?email=evan@fireinspace.net&first_name=Evan&last_name=Farinholt
+
+*/
+
+$email		= $_GET['email'];
+$first_name	= $_GET['first_name'];
+$last_name	= $_GET['last_name'];
+
+
+/*
+this declairs each parameter in our url as its own variable. 
+$_GET is a global variable in php that takes the value of the url parameter inside the brackets 
+so in this example, $_GET['email'] will be 'evan@fireinspace.net'
+*/
+
+header('Content-Type: application/json');
+// assigning this json header ensures that our response will be parsed properly by a third party
+
 echo json_encode(array(
-					'users' => array(
-						'user' => array(
-							'reg_info' => array(
-								'user_first' => $user_data['user_first'],
-								'user_last' => $user_data['user_last'],
-								'user_email' => $user_data['user_email']
-							),
-							'sys_info' => array(
-								'user_token' => $user_data['user_token'],
-								'user_status' => $user_data['user_status'],
-								'user_type' => $user_data['user_type'],
-								'user_reg_ip' => $user_data['user_reg_ip'],
-								'user_last_login' => $user_data['user_last_login'],
-								'user_log_ip' => $user_data['user_log_ip'],
-								'user_timestamp' => $user_data['user_timestamp'],
-							)
-						)
-					)
-				));
+// encodes the array as json. 'echo' prints it to the page
+	'users' => array(
+		'user' => array(
+			'info' => array(
+				'first_name' => $first_name,
+				'last_name' => $last_name,
+				'email' => $email
+			)
+		)
+	)
+));
+
+/*
+
+passing the POST url will result in the json object below:
+
+users: {
+	user: {
+		first_name: 'evan',
+		last_name: 'farinholt',
+		email: 'evan@fireinspace.net'
+	}
+}
+
+
+*/
+?>
